ed87b598ba6391bc9182d9aba9f045e2cdd9c0bc
RahulJava-1 <rahultripathirudra@gmail.com>
2023-11-28 14:33:34 +0530

    second

---
+++ calculator.py
@@ -0,0 +1,7 @@
+x=5
+y=4
+sum=x+y
+sub=x-y
+mul=x*y
+div=x/y #for return floatint point value and // for return integer value
+print("Sum = ",sum," Subtract = ",sub," Multiply = ",mul, " Divide = ",div)
