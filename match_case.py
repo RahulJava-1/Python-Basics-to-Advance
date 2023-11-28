1c0e40e69921832e05f8e871294ca48801564bd1
RahulJava-1 <rahultripathirudra@gmail.com>
2023-11-28 23:32:12 +0530

    for

--- match_case.py
+++ match_case.py
@@ -1,6 +1,6 @@
 #It's looking like switch case statement in C, C++ or Java
 #there is no need of break
-
+# we can use 3 default cases
 
 x = int(input("Enter the number: "))
 
@@ -9,5 +9,10 @@ match x:
         print("X is 0")
     case 1:
         print("case 1")
+
+    case _ if x!=10:
+        print(x, "is not 20")
+    case _ if x!=20:
+        print(x, "is not 10")
     case _:         #default case
         print(x)
